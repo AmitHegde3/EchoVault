94bebb08f284140ff83d166a01b32b781b2e5f50
Amit Hegde <amit.is21@bmsce.ac.in>
2024-03-10 19:39:12 +0530

    Update Express validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,34 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User');
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { query, validationResult } = require("express-validator");
 
+router.use(express.json());
 // Create a user uisng: POST "/api/auth/" . Doesnt require Auth
 
-router.post('/', (req,res) => {
+router.post(
+  "/",
+  [
+    query("name", "Enter a valid Name").isLength({ min: 5 }),
+    query("email", "Enter a vlid Email").isEmail(),
+    query("password", "Enter a valid Password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      res.send({ errors: result.array() });
+      return; //This was thnecause of error
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => console.log(err));
     console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body)
-     
-})
+    //   res.send(req.body);
+  }
+);
 
-module.exports = router
+module.exports = router;
