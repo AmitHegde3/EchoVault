19ef219f4f083279c72252a624c0eb3c4d75da0b
Amit Hegde <amit.is21@bmsce.ac.in>
2024-03-11 13:42:58 +0530

    Update notes ednpoints and API

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,51 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { query, validationResult } = require("express-validator");
 
-router.get('/', (req,res) => {
-    
-    res.json([])
-})
+// Route 1: Get all the notes using : GET "/api/auth/fetchallnotes" . Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error!");
+  }
+});
 
-module.exports = router
+// Route 2: Add a new note using : POST "/api/auth/addnote" . Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    query("title", "Enter a valid Title").isLength({ min: 3 }),
+    query("description", "Enter a valid Description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        res.send({ errors: result.array() });
+        return; //This was the cause of error
+      }
+
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const saveNote = await note.save();
+      res.json(saveNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error!");
+    }
+  }
+);
+
+module.exports = router;
