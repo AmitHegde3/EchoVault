acc2f66aa196ba80684f0f3e8344e9b957145f9e
Amit Hegde <amit.is21@bmsce.ac.in>
2024-03-12 17:24:17 +0530

    Update notes context

--- backend/index.js
+++ backend/index.js
@@ -12,5 +12,5 @@ app.use('/api/notes', require('./routes/notes.js'))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Echo Vault backend listening at http://localhost:${port}`)
 })
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,9 +2,31 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
+  const notesInitial = [
+    {
+      "_id": "65eebc99a60453caa22be944",
+      "user": "65eddf697ad7da411727a498",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2024-03-11T08:11:05.700Z",
+      "__v": 0
+    },
+    {
+      "_id": "65eec377e25ab0f86a3a373c",
+      "user": "65eddf697ad7da411727a498",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2024-03-11T08:40:23.815Z",
+      "__v": 0
+    }
+  ]
+
+  const [notes, setNotes] = useState(notesInitial)
   
   return (
-    <NoteContext.Provider value={{}}>
+    <NoteContext.Provider value={{notes,setNotes}}>
       {props.children}
     </NoteContext.Provider>
   );
