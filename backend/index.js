48a8ed4a6373cb31d3cbf15e8db8a66dfbb53311
Amit Hegde <amit.is21@bmsce.ac.in>
2024-03-13 18:34:55 +0530

    Update API connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const connectToMongo = require("./db")
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 5000
 
 connectToMongo();
 app.use(express.json())
 
+app.use(cors())
 // Avilable Routes
 app.use('/api/auth', require('./routes/auth.js'))
 app.use('/api/notes', require('./routes/notes.js'))
