bcdf3fd5ca9ed3d603ca33bfe82e2bfa8234042e
Amit Hegde <amit.is21@bmsce.ac.in>
2024-03-13 09:48:34 +0530

    update delete and update icons, Update Alert box

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <>
       <NoteState>
         <Navbar />
+        <Alert message={"This is Amazing"} />
         <div className="container">
-        <Routes>
-         
+          <Routes>
             <Route path="/">
               <Route index element={<Home />} />
               <Route exact path="about" element={<About />} />
             </Route>
-         
           </Routes>
-          </div>
+        </div>
       </NoteState>
     </>
   );
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,11 +4,13 @@ const NoteItem = (props) => {
   let Note = props.note;
   return (
     <div className="col-md-3">
-      <div class="card my-3">
-        <div class="card-body">
-          <h5 class="card-title">{Note.title}</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p class="card-text">{Note.description}</p>
+      <div className="card my-3">
+        <div className="card-body">
+          <h5 className="card-title">{Note.title}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p className="card-text">{Note.description}</p>
+          <i className="fa-solid fa-trash mx-2"></i>
+          <i className="fa-solid fa-pen-to-square fa-beat mx-2"></i>
         </div>
       </div>
     </div>
